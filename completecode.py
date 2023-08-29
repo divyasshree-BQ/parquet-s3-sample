f935ce8d0d88102e2523810821423f8b8216149c
divyasshree-BQ <124589840+divyasshree-BQ@users.noreply.github.com>
2023-08-29 17:45:32 +0530

    Update completecode.py

--- completecode.py
+++ completecode.py
@@ -4,17 +4,17 @@ import pyarrow.parquet as pq
 import pandas as pd 
 import parquet
 
-s3 = boto3.client('s3', aws_access_key_id='key id', aws_secret_access_key='key', region_name='us-east-1')
+s3 = boto3.client('s3', aws_access_key_id='your id', aws_secret_access_key='your key', region_name='us-east-1')
 bucket_name = 'parquet-bsc'
 
 
-bsc_local_path = 'Your Path/sample.parquet'
-bsc_key='dex_trades_tx/2020-09-12_410000_C56AEF483BA69257_5000.parquet'
+bsc_local_path = 'C:/Users/divyn/Downloads/sample.parquet'
+bsc_key='dex_trades_tx/2020-09-12_410000_88573E9FA803AF8D_10000.parquet'
 
-filename='2020-09-12_410000_C56AEF483BA69257_5000.parquet'
+filename='2020-09-12_410000_88573E9FA803AF8D_10000.parquet'
 
 file_obj = s3.download_file(bucket_name, bsc_key, bsc_local_path)
 
 df = pd.read_parquet(bsc_local_path)
 
-print(df.head()) 
+df.to_csv('parquet_output.csv', index=False)
